0a5ec6a4fbaf01b638332d59cb97000365aba474
ThePatman3 <patrick.vanderbend@student.hu.nl>
2018-11-05 15:52:35 +0100

    Updated initGameControl
    Added WIP version of initGameControl

---
+++ InitGameControl.cpp
@@ -0,0 +1,57 @@
+#include "hwlib.hpp"
+#include "rtos.hpp"
+
+#include "IKeyboardListener.hpp"
+#include "message.hpp"
+#include "Display.hpp"
+#include "EncodeDecodeMSG.hpp"
+#include "SendIrMessageControl.hpp"
+
+
+void InitGameControl::KeyboardKeyPressed(char Key){
+	KeyPressedInitQueue.write(Key);
+}
+void InitGameControl::main(){
+	while(true){
+		switch(currentState){
+			case WaitForKeyC:
+				wait(KeyPressedInitQueue);
+				pressedKey = KeyPressedInitQueue.read();
+				if(pressedKey == 'c'){
+					command = 0;
+					currentState = WaitForNumber;
+				}
+			case WaitForNumber:
+				displayControl.DisplayString(command, ENTIRE_SCREEN);
+				wait(KeyPressedInitQueue);
+				pressedKey = KeyPressedInitQueue.read();
+				if(pressedKey >= '0' && pressedKey <= '9'){
+					command *= 10;
+					command += (pressedKey - '0');
+					if(command > 15) command = 0;
+				}else if(pressedKey == '#' && command != 0){
+					encodedMessage = encodeDecoder.EncodeMessage(0, command);
+					currentState = WaitForHash;
+				}
+			case WaitForHash:
+				wait(KeyPressedInitQueue);
+				pressedKey = KeyPressedInitQueue.read();
+				if(pressedKey == '*'){
+					encodedMessage = encodeDecoder.EncodeMessage(0, 0);
+					displayControl.DisplayString("StartCommand", ENTIRE_SCREEN);
+					sendIrMessageControl.sendMessage(encodedMessage);
+					currentState = StartCommand;
+				}else if(pressedKey == '#'){
+					sendIrMessageControl.sendMessage(encodedMessage);
+				}
+			case StartCommand:
+				wait(KeyPressedInitQueue);
+				pressedKey = KeyPressedInitQueue.read();
+				if(pressedKey == 'c'){
+					currentState = WaitForNumber;
+				}else if(pressedKey = '*'){
+					sendIrMessageControl.sendMessage(encodedMessage);
+				}
+		}
+	}
+}
--- InitGameControl.hpp
+++ InitGameControl.hpp
@@ -1,15 +1,37 @@
 #ifndef GAMEMASTER_INITGAMECONTROL_HPP
 #define GAMEMASTER_INITGAMECONTROL_HPP
 
+#include "hwlib.hpp"
 #include "rtos.hpp"
 
 #include "IKeyboardListener.hpp"
-// De Message ADT is niet beschreven in ADTs.hpp maar deze file gebruikt wel de Message ADT...
-// De juiste file moet dus nog #include worden.
+#include "message.hpp"
+#include "Display.hpp"
+#include "EncodeDecodeMSG.hpp"
+#include "SendIrMessageControl.hpp"
 
 class InitGameControl: public rtos::task<>, public IKeyboardListener{
+private:
+	rtos::channel<char, 10> KeyPressedInitQueue;
+	
+	enum InitGameControlStates = { WaitForKeyC, WaitForNumber, WaitForHash, StartCommand };
+	InitGameControlStates currentState;
+	
+	DisplayControl& displayControl;
+	EncodeDecodeMSG& encodeDecoder;
+	SendIrMessageControl& sendIrMessageControl;
+	
+	char pressedKey;
+	uint8_t command;
+	uint16_t encodedMessage;
+	
 public:
+	InitGameControl(const unsigned int priority, const char* taskName, DisplayControl& _displayControl, EncodeDecodeMSG& _encodeDecoder, SendIrMessageControl& _sendIrMessageControl):
+		task(priority, taskName), KeyPressedInitQueue(this, "KeyPressedInitQueue"), displayControl(_displayControl), encodeDecoder(_encodeDecoder),
+		sendIrMessageControl(_sendIrMessageControl){currentState = WaitForKeyC;}
+	
 	void KeyboardKeyPressed(char Key);
+	void main();
 };
 
 #endif
