e4a56be97930592ce358dd3b8720c3bd78dbe934
ThePatman3 <patrick.vanderbend@student.hu.nl>
2018-11-22 14:26:21 +0100

    Added doxygen

--- InitGameControl.hpp
+++ InitGameControl.hpp
@@ -10,6 +10,10 @@
 #include "SendIrMessageControl.hpp"
 #include "DisplayControl.hpp"
 
+/// \brief Control object that manages the initialization of the game.
+/// \details InitGameControl manages the gamemaster: register the gametime,
+/// sending the gametime and sending the startcommand.
+/// This class inherits from rtos::task<> and IKeyboardListener.
 class InitGameControl: public rtos::task<>, public IKeyboardListener{
 private:
 	rtos::channel<char, 10> KeyPressedInitQueue;
@@ -26,10 +30,19 @@ private:
 	uint16_t encodedMessage;
 	void main();
 public:
+	/// \brief InitGameControl constructor
+	/// \details Constructs an InitGameControl object with the specified parameters.
+	/// @param priority The priority of the task.
+	/// @param taskName The name of the task, used in debugging.
+	/// @param _displayControl A reference to the DisplayControl object this class will use to display data.
+	/// @param _encodeDecoder A reference to the EncodeDecodeMSG object this class will use to encode its ir-messages.
+	/// @param _sendIrMessageControl A reference to the SendIrMessageControl object this class will use to send its ir-messages.
 	InitGameControl(const unsigned int priority, const char* taskName, DisplayControl& _displayControl, EncodeDecodeMSG& _encodeDecoder, SendIrMessageControl& _sendIrMessageControl):
 		task(priority, taskName), KeyPressedInitQueue(this, "KeyPressedInitQueue"), displayControl(_displayControl), encodeDecoder(_encodeDecoder),
 		sendIrMessageControl(_sendIrMessageControl){currentState = WaitForKeyC;}
 	
+	/// \brief function to let this class know a key on its keyboard is pressed.
+	/// @param Key Specifies which key is pressed.
 	void KeyboardKeyPressed(char Key);
 };
 
